0d3f301dabb298c76ee4045601a1c261335cf784
FatAnorexic <54606095+FatAnorexic@users.noreply.github.com>
2023-11-24 11:24:11 -0500

    Add memory object and modify setOperand function
    Added the memory object which will store input values and operand for use in the calculation
    Also modified the setOperand function to run a check if the operator value is an empty string
    Future expansion will create an else clause that fires the calculate function if it is not
    empty. This is in keeping with the sequenctiality request of the assignment in allowin numerical
    values and operations to daisy chain and stack.

--- calculator.js
+++ calculator.js
@@ -10,9 +10,22 @@ screen.textContent='0';
 numbers.forEach(number => number.addEventListener('click',()=>setNumber(number.textContent)))
 operands.forEach((op)=> op.addEventListener( 'click', ()=> setOperand(op.textContent)));
 
+let memory={
+    firstInput:0,
+    operator:'',
+    secondInput:0,
+}
+
+//We check the object here to complete two tasks. 1) to seperate numerical values from operators
+//and 2)If the value operator is not an empty string, the function will pass to the calculate
+//function in order to keep the sequentiality request within the assignment. 
 function setOperand(operand){
-    console.log(operand);
-    screen.textContent+=operand;
+    if(memory.operator===''){
+        memory.firstInput=screen.textContent;
+        memory.operator=operand;
+        screen.textContent='0';
+        console.log(`Input: ${memory.firstInput}, Operator: ${memory.operator}`);
+    }
 }
 
 function setNumber(number){
