556bcbb2d44d9d89a89095105d57f6c8ed185dd7
FatAnorexic <54606095+FatAnorexic@users.noreply.github.com>
2023-11-26 10:29:59 -0500

    Clean code for legibility and brevity
    While a lot has been moved around and the order of things changed, the basic principles
    of the calculator remain mostly the same as can be seen when looking at the changelog in code
    The major differences added were a rounding function to prevent numerical overflow and the
    implementation(proper) of the operate function which now takes values instead of grabbing
    directly from the global object memory. Everything else is short hand or a more clean version
    of an already existing function. Credit must be given to @michalosman for inspiration in the
    implemenation of a reset function and boolean global variable. As well as the round function which
    is borrowed directly from his solution.

--- calculator.js
+++ calculator.js
@@ -1,9 +1,17 @@
 
+//Object for storing strings of values. These strings will then be converted to Ints or floats
+//depending on the type of input given.
+let memory={
+    firstInput:'',
+    secondInput:'',
+    operator: '',
+}
+let click=false;
+let reset=false;
+
 const operands=document.querySelectorAll('.operand');
 const numbers=document.querySelectorAll('.number');
-
 const screen=document.getElementById('currentScreen');
-
 
 //Initializer for screen.textContent
 screen.textContent='0';
@@ -12,101 +20,76 @@ screen.textContent='0';
 numbers.forEach(number => number.addEventListener('click',()=>setNumber(number.textContent)))
 operands.forEach((op)=> op.addEventListener( 'click', ()=> setOperand(op.textContent)));
 
-let click=false;
 //Event listenter for = button
 document.getElementById('equal').addEventListener('click', ()=>{
     //so we dont fire the calculate function without having an operand
-    if(memory['operator']!==''){click=true; calculate();}
+    if(memory['operator']!==''){
+        calculate();
+        click=true;
+    }
 });
-
-//Object for storing strings of values. These strings will then be converted to strings or floats
-//depending on the type of input given.
-let memory={
-    firstInput:'',
-    operator:'',
-    secondInput:'',
-}
 
 //We check the object here to complete two tasks. 1) to seperate numerical values from operators
 //and 2)If the value operator is not an empty string, the function will pass to the calculate
 //function in order to keep the sequentiality request within the assignment. 
 function setOperand(operand){
-    if(memory['operator']===''){
-        memory['operator']=operand;
-        screen.textContent='0';
-        console.log(`Operator: ${memory['operator']}`);
-    }else if(click===true){
-        memory['operator']=operand;
-        memory['secondInput']='';
-        screen.textContent='0';
-        click=false;
-        console.log(`Operator: ${memory['operator']}, Equals Button: ${click}`);
-    }else{
-        calculate();
-        memory['operator']=operand;
-        memory['secondInput']='';
-        // screen.textContent='0';
-        click=false;
-        console.log(`Operator: ${memory['operator']}`);
-    }
+    if(memory['operator']!=='' && !click) {calculate();}
+    else if(memory['operator']!=='' && click) {click=false;}
+    
+    memory['firstInput']=screen.textContent;
+    memory['operator']=operand;
+    console.log(`Operand: ${memory["operator"]}, Input: ${memory['firstInput']}`)
+    reset=true;
 }
 
 function setNumber(number){
-    let string=screen.textContent
     //Prevents 0's from stacking on values: IE 01123-> 1123
-    if(string.length===1 && screen.textContent==='0'){
-        screen.textContent=number;
-    }else{
-        screen.textContent+=number;
-    }
-
-    //Checks for an empty operator to determine if number is appended to 
-    //first or second input in the memory object. 
-    if(firstInput()){
-        memory['firstInput']+=number;
-    }else{
-        memory['secondInput']+=number;
-    }
+    screen.textContent==='0' || reset ? clearScreen(number):screen.textContent+=number;
 }
 
-function firstInput(){
-    return memory['operator']==='';
+function clearScreen(number){
+    screen.textContent=number;
+    reset=false;
 }
 
-
-
-//function for determining if calculating for int/float numbers
 function calculate(){
     //temp filler
-    operate();
-}
-function operate(){
-    let x=parseInt(memory['firstInput']);
-    let y=parseInt(memory['secondInput']);
-    console.log(x);
-    console.log(y);
+    if(memory['operator']===''|| reset) {return;}
 
-    switch(memory['operator']){
+    if(memory['operator']==='/' && screen.textContent==='0'){
+        alert(`Naughty Naughty!`);
+        return;
+    }
+    memory['secondInput']=screen.textContent;
+    screen.textContent=roundAnswer(
+        operate(memory['operator'], memory['firstInput'], memory['secondInput'])
+        );
+}
+
+function roundAnswer(number){
+    return Math.round(number*1000)/1000;
+}
+
+
+function operate(operator, first, second){
+    first=Number(first);
+    second=Number(second);
+    console.log(first);
+    console.log(second);
+
+    switch(operator){
         case '+':
-            screen.textContent=add(x,y);
-            break;
+            return add(first,second);
         case '-':
-            screen.textContent=subtract(x,y);
-            break;
+            return subtract(first,second);
         case '*':
-            screen.textContent=multiply(x,y);
-            break;
+            return multiply(first,second);
         case '/':
-            screen.textContent=divide(x,y);
-            if(screen.textContent===''){
-                memory['operator']='';
-                screen.textContent='0'
-            }
-            break;
-    }   
-    memory['firstInput']=screen.textContent;
-    screen.textContent=memory['firstInput']
-    console.log(memory['firstInput']);
+            if(second===0) {return null;}
+            return divide(first,second);
+        default:
+            return null;
+    }
 }
 
 //Basic math functions. 
@@ -120,17 +103,13 @@ function multiply(x,y){
     return x*y;
 }
 function divide(x,y){
-    return y===0 ? alert(`Naughty Naughty!`): x/y;
+    return x/y;
 }
 
 
 //function for deleting entries on a screen
 document.querySelector('.delete').addEventListener('click',()=>{
-    if(screen.textContent==='0'){
-        screen.textContent='0';
-    }
-    screen.textContent=screen.innerText.slice(0,-1);
-
+    screen.textContent=screen.textContent.toString().slice(0,-1);
     //we want a base value of zero, always
     if(screen.textContent===''){
         screen.textContent='0';
@@ -142,6 +121,5 @@ document.querySelector('.clear').addEventListener('click',()=>{
     memory['operator']='';
     memory['secondInput']='';
     click=false;
-    console.log(`Input: ${memory['firstInput']}, Operator: ${memory['operator']}, Input: ${memory['secondInput']}`);
     screen.textContent='0';
 });
